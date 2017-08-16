93ab5737be827d90f9956d25af87e0f5894c6141
Javier María Cortés Conde Titó <javiercortescondetito@gmail.com>
2017-08-16 11:49:14 -0300

    agregue un grafico mas y comentarios sobre lo que nos falta o que tendriamos que hacer, basicamente releer la clase... tendiramos que hacer un grupo de telegram

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -11,5 +11,39 @@ sol = sns.tsplot(time=df['Atleta'], data=df['tiempo_sol'], interpolate=False, co
 nublado = sns.tsplot(time=df['Atleta'], data=df['tiempo_nublado'], interpolate=False, color="red")
 
 lluvia = sns.tsplot(time=df['Atleta'], data=df['tiempo_lluvia'], interpolate=False, color="green")
+
 sns.plt.legend([sol, nublado, lluvia], labels=["sol","nublado","lluvia"])
+
 sns.plt.show()
+"""
+esta seria la fase de limpiar.
+dropee el ateta que tenia el dato, pero queria dropear solo ese dato feo...
+podemos asumir que se paso mal y el tiempo es 18.9 o poner el promedio, va que se yo
+"""
+
+df.drop(df[df.tiempo_lluvia > 180.0].index, inplace=True)
+
+sol = sns.tsplot(time=df['Atleta'], data=df['tiempo_sol'], interpolate=False, color="blue")
+
+nublado = sns.tsplot(time=df['Atleta'], data=df['tiempo_nublado'], interpolate=False, color="red")
+
+lluvia = sns.tsplot(time=df['Atleta'], data=df['tiempo_lluvia'], interpolate=False, color="green")
+
+sns.plt.legend([sol, nublado, lluvia], labels=["sol","nublado","lluvia"])
+
+sns.plt.show()
+
+
+"""
+Nos falta definir que cosas usar, para mi un tstudent tipo muestra independtiente
+para ver cual es la prob de que lluvia y sol y nublado (tomados de dos9 tengan la
+media... posta copairse del powerpoint(acordarse de chequear las hipotesis)
+
+
+Tendriamos que releer los test de rank sum, mannwhitney u y wilcoxon, que serian
+los que no estabamos cuando los explico...
+
+
+y el de permutaciones, que permutamos?los tiempos en nublado sol y lluvia?
+
+"""
